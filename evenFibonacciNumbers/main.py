d7ed6a92df1a2b8de5192dfeabece771b5bf031d
ahmetbaglan <ahmet.baglan1@gmail.com>
2016-07-22 23:13:48 +0300

    #2 problem solved

--- evenFibonacciNumbers/main.py
+++ evenFibonacciNumbers/main.py
@@ -5,15 +5,15 @@ def getEvenFibTotal(r):
     totalEven = 2
     a = 1
     b = 2
-    print a
-    print b
-    for i in range(r-2):
+    while(b<r):
         temp = getFib(a,b)
         a = b
         b = temp
         if(temp % 2 == 0):
+            if(b>r):
+                break
+            print temp
             totalEven+=temp
-        print temp
     print "And the toatal is", totalEven
 
-getEvenFibTotal(10)
+getEvenFibTotal(4000000)
