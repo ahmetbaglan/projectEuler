eee105d374e79f1e07c154b28e2f37d9d768735c
ahmetbaglan <ahmet.baglan1@gmail.com>
2016-07-25 22:40:14 +0300

    #12 solved

---
+++ #12_TriangleNum/triangle.py
@@ -0,0 +1,37 @@
+def m(n):
+    k = n
+    d ={}
+    index = 1
+    max = 2
+    while k>=2:
+        index+=1
+        if(k % index == 0):
+            if(index in d.keys()):
+                d[index]+=1
+            else:
+                d[index] = 1
+            if(index>max):
+                max = index
+            k = k/index
+            index = 1
+    return d
+
+def numDiv(d):
+    divisor = 1
+    for i in d:
+        divisor *= (d[i]+1)
+    return divisor
+
+
+def bul(k):
+    a = 0
+    n = 1
+    while k>a:
+        sayi = n * (n+1) / 2
+        d = m(sayi)
+        a = numDiv(d)
+        n = n+1
+    print d
+    return sayi
+
+print bul(500)
