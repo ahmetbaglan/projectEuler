77fdc83074fda622b1840c1e183f267955e0dbea
ahmetbaglan <ahmet.baglan1@gmail.com>
2016-07-26 22:34:25 +0300

    #16 Solved

---
+++ #16_Mult/mult.py
@@ -0,0 +1,25 @@
+def mult(a,b):
+    m = str(a)
+    now = 0
+    l = []
+    eldevar = 0
+    for i in range(b-1):
+
+        for k in range(len(m)-1,-1,-1):
+            now = int(m[k]) * a + eldevar
+            eldevar = now/10
+            l.append(now%10)
+            if(k == 0 and eldevar != 0):
+                l.append(eldevar)
+                eldevar = 0
+        m = l[::-1]
+        l =[]
+    sum = 0
+    for l in m:
+        sum+=l
+
+    return sum
+
+print mult(2,1000)
+
+
